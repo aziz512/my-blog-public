9d3599f03a7ca4a8c64f985e88839a0dbdbe08ec
Aziz Yokubjonov <aziz.yokubjonov@gmail.com>
2020-05-20 00:29:00 -0400

    update

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-jekyll build
-cd _site
-git init
-git remote add origin https://github.com/aziz512/my-blog-public
-git checkout -b gh-pages
-git add .
-git commit -m 'update'
-git push -f origin gh-pages
